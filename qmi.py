a5dee18948babb5bed8003cb73e1c19233ec1d8e
BiatuAutMiahn <BiatuAutMiahn@outlook.com>
2021-06-06 22:58:56 +0000

    [qmi] Implemented queueing for queries. [stn] Implemented queue for queries. [stn] Implemented watch for OBD readings

--- qmi.py
+++ qmi.py
@@ -1,6 +1,9 @@
 import serial
 import time
 import pynmea2
+import random
+import string
+from collections import OrderedDict
 
 Magic = "ftbOsM6KwAn98Y58"
 Alias = "qmi"
@@ -21,44 +24,85 @@ config = {
         'AT+QGPS=2,30,50,0,1'
     ]
 }
+queue=None
 
-def _query(c):
-    node.lock=True
-    node.Serial.write((c+'\r').encode())
-    ret=node.Serial.readlines()
-    node.lock=False
-    return ret
+def genid():
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
 
-def _show_query(c):
-    ret=_query(c)
-    for l in ret:
-        if l==b'\r\n':
-            continue
-        print(l.decode())
+def _query(c,wait=False,ret_result=False,echo=False,log=False,do_proc=False):
+    global queue
+    global tmp
+    qid=genid()
+    q={'stdin':c,'stdout':None}
+    if ret_result:
+        wait=True
+    if wait:
+        if echo:
+            print(c)
+        if log:
+            logging.info("["+node.name+"]:\tQMI -> "+c)
+    queue[0][qid]=q
+    if not wait:
+        return qid
+    while node.init==5 or node.init==4:
+        if qid in queue[1]:
+            break
+        if do_proc:
+            _proc_queue()
+        time.sleep(0.1)
+    if echo or log:
+        tmp=""
+        for l in queue[1][qid]['stdout']:
+            if l==b'\r\n':
+                continue
+            tmp+=l.decode()
+        if echo:
+            print(tmp)
+        if log:
+            logging.info("["+node.name+"]:\tQMI <- "+tmp)
+    if ret_result:
+        return (qid,queue[1][qid])
+    return qid
+
+
+def _proc_queue():
+    global queue
+    node.queue=queue
+    for k in queue[0].keys():
+        node.Serial.write((queue[0][k]['stdin']+'\r').encode())
+        queue[0][k]['stdout']=node.Serial.readlines()
+        queue[1][k]=queue[0].pop(k)
 
 def __init__(n,l):
     global node
     global logging
     global config
+    global queue
     node=n
     logging=l
     node.id=Magic
     node.config=config
     node.lock=False
     node.query=_query
-    node.show_query=_show_query
+    node.queue=queue
+    node.loop_interval=0.25
+    node.gps_fix=None
+    
+    queue=[OrderedDict(),OrderedDict()]
     node.Serial=serial.Serial(port=config['device'],baudrate=config["baudrate"],timeout=config["timeout"])
-    # node.Serial.port=config['device']
-    if node.Serial.is_open:
+    if node.Serial.is_open: # ensure closed before opening
         node.Serial.close()
     node.Serial.open()
     for c in config['init']:
-        logging.info("["+node.name+"]:\tQMI <- "+c)
-        ret=_query(c)
-        for l in ret:
-            if l==b'\r\n':
-                continue
-            logging.info("["+node.name+"]:\tQMI -> "+l.decode())
+        _query(c,wait=True,do_proc=True,log=True)
         #time.sleep(0.1)
     time.sleep(1)
     logging.info("["+node.name+"]:\tInitialized")
+
+# Replace
+def __loop__(self):
+    qid=_query('AT+QGPSLOC?')
+    _proc_queue()
+    if qid in queue[1]:
+        r=queue[1].pop(qid)
+        node.gps_fix=r['stdout'][1].decode()
--- stn.py
+++ stn.py
@@ -1,42 +1,97 @@
 import obd
 import RPi.GPIO as gpio
 from gpio_pin  import RPI_SHUTDN
+import random
+import string
+from collections import OrderedDict
+
 Magic = "5KZMmWv2XOnD4BKD"
 Alias = "stn"
 node=None
 logging=None
 config={'device': '/dev/serial0', 'baudrate': 9600, 'timeout': 1}
 
-def _query(**kwargs):
-    node.lock=True
-    ret=node.OBD.query(**kwargs)
-    node.lock=False
-    return ret
+queue=None
+watcher={}
+def genid():
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+
+def _query(c,wait=False,ret_result=False,echo=False,log=False,do_proc=False):
+    global queue
+    global tmp
+    qid=genid()
+    q={'stdin':c,'stdout':None}
+    if ret_result:
+        wait=True
+    if wait:
+        if echo:
+            print(c)
+        if log:
+            logging.info("["+node.name+"]:\STN -> "+c)
+    queue[0][qid]=q
+    if not wait:
+        return qid
+    while node.init==5 or node.init==4:
+        if qid in queue[1]:
+            break
+        if do_proc:
+            _proc_queue()
+        time.sleep(0.1)
+    if echo or log:
+        tmp=""
+        for l in queue[1][qid]['stdout']:
+            if l==b'\r\n':
+                continue
+            tmp+=l.decode()
+        if echo:
+            print(tmp)
+        if log:
+            logging.info("["+node.name+"]:\STN <- "+tmp)
+    if ret_result:
+        return (qid,queue[1][qid])
+    return qid
+
+
+def _proc_queue():
+    global queue
+    node.queue=queue
+    for k in queue[0].copy().keys():
+        # ret=node.OBD.query(k)
+        queue[0][k]['stdout']=node.OBD.query(queue[0][k]['stdin'])
+        queue[1][k]=queue[0].pop(k)
 
 def __init__(n,l):
     global node
     global logging
     global config
+    global queue
     node=n
     logging=l
     node.id=Magic
     node.voltage=None
     node.loop_interval=1
-    gpio.setwarnings(False)
-    gpio.setmode(gpio.BOARD)
-    gpio.setup(RPI_SHUTDN, gpio.IN)
-    node.OBD = obd.OBD(config['device'])
     node.lock=False
     node.query=_query
     node.config=config
+    node.watch={'ELM_VOLTAGE':None,'ELM_VERSION':None}
+    queue=[OrderedDict(),OrderedDict()]
+    node.OBD = obd.OBD(node.config['device'])
+    gpio.setwarnings(False)
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(RPI_SHUTDN, gpio.IN)
     logging.info("["+node.name+"]:\tInitialized")
 
 def __deinit__(self):
     node.OBD.close()
 
 def __loop__(self):
+    for c in node.watch.copy().keys():
+        watcher[c]=_query(obd.commands[c])
     try:
-        if not node.lock:
-            node.voltage=node.OBD.query(obd.commands.ELM_VOLTAGE).value
+        _proc_queue()
     except Exception as e:
         node.handle_exception(e)
+    for c in node.watch.copy().keys():
+        if watcher[c] in queue[1]:
+            node.watch[c]=queue[1][watcher[c]]['stdout']
+        # node.voltage=node.OBD.query(obd.commands.ELM_VOLTAGE).value
